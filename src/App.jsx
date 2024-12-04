14e764d79bf2a452ee1f80a4f0a8733432dab813
Magesh.B <Mageshmca2001@gmail.com>
2024-12-04 14:35:46 +0530

    new change da

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Sidebar, { SidebarItem } from "./components/Sidebar"
+
+function App() {
+
+  return (
+    <>
+      <div className="flex">
+        <Sidebar>
+          
+        </Sidebar>
+      </div>
+    </>
+  )
+}
+
+export default App
+
+
